152be5014b3ac6ff62b098f253c83971227ca439
EC2 Default User <ec2-user@ip-172-31-7-110.us-west-2.compute.internal>
2024-12-08 19:18:01 +0000

    terminate instances

--- terragrunt/destroy.sh
+++ terragrunt/destroy.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
 echo shutdown instances in ASG
-#aws autoscaling update-auto-scaling-group \
-#    --auto-scaling-group-name $(aws --region us-west-2 autoscaling describe-auto-scaling-groups|jq -r '.AutoScalingGroups[].AutoScalingGroupName') \
-#    --min-size 0 \
-#    --desired-capacity 0 \
-#    --max-size 0 \
-#    --region us-west-2
+asg_name=$(aws --region us-west-2 autoscaling describe-auto-scaling-groups|jq -r '.AutoScalingGroups[].AutoScalingGroupName')
+
+ec2_instance_ids=$(aws autoscaling describe-auto-scaling-instances \
+  --query "AutoScalingInstances[?AutoScalingGroupName=='${asg_name}'].InstanceId" \
+  --output text)
+
+aws autoscaling set-instance-protection \
+  --auto-scaling-group-name "${asg_name}" \
+  --instance-ids ${ec2_instance_ids} \
+  --no-protected-from-scale-in
+
+aws autoscaling update-auto-scaling-group \
+    --auto-scaling-group-name $asg_name \
+    --min-size 0 \
+    --desired-capacity 0 \
+    --max-size 0 \
+    --region us-west-2
+
+aws ec2 terminate-instances --instance-ids $ec2_instance_ids --region us-west-2
+
+# Drain each container instance
+cluster_name=hello-world-cluster
+ecs_instance_ids=$(aws ecs list-container-instances --cluster ${cluster_name} --query "containerInstanceArns" --output text)
+
+for instance_id in ${ecs_instance_ids}; do
+  echo drain $instance_id
+  aws ecs update-container-instances-state \
+    --cluster $cluster_name \
+    --container-instances ${instance_id} \
+    --status DRAINING
+done
 
 echo delete images in ecr
 aws ecr list-images --repository-name hello-world --query 'imageIds[*]' --output json > images-to-delete.json
--- terragrunt/setup.sh
+++ terragrunt/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sudo systemctl start docker >/dev/null 2>&1
+sudo chown ec2-user /var/run/docker.sock
 terragrunt run-all apply --terragrunt-non-interactive
